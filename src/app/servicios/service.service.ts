5ee154e4439ca9ef9b37f160a20b4ad03f9308ac
jorge heidenreich <jmjhjorge@gmail.com>
2023-02-18 19:26:44 -0300

    Cambio links del Back-End

--- src/app/servicios/service.service.ts
+++ src/app/servicios/service.service.ts
@@ -12,19 +12,19 @@ export class ServiceService {
 
   //Metodo GET
   getEmplados(): Observable<Empleado[]> {
-    return this.http.get<Empleado[]>('https://backend-lista-empleados-production.up.railway.app/api/empleados')
+    return this.http.get<Empleado[]>('https://backend-lista-empleados-vercel-iktc1qssd-jorgeheiden.vercel.app/api/empleados')
   }
   //Metodo POST
   nuevoEmpleado(empleado:Empleado): Observable<Empleado>{
-    return this.http.post<Empleado>('https://backend-lista-empleados-production.up.railway.app/api/empleados', empleado)
+    return this.http.post<Empleado>('https://backend-lista-empleados-vercel-iktc1qssd-jorgeheiden.vercel.app/api/empleados', empleado)
   }
   //Metodo PUT
   editarEmpleado(empleado: Empleado): Observable<Empleado>{
-    const url = `${'https://backend-lista-empleados-production.up.railway.app/api/empleados'}/${empleado.idempleados}`
+    const url = `${'https://backend-lista-empleados-vercel-iktc1qssd-jorgeheiden.vercel.app/api/empleados'}/${empleado.idempleados}`
     return this.http.put<Empleado>(url, empleado)
   }
   //Metodo DELETE
   eliminarEmpleado(id:number){
-    return this.http.delete(`${'https://backend-lista-empleados-production.up.railway.app/api/empleados'}/${id}`)
+    return this.http.delete(`${'https://backend-lista-empleados-vercel-iktc1qssd-jorgeheiden.vercel.app/api/empleados'}/${id}`)
   }
 }
